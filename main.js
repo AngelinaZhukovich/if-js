735834a9f8ddd5cfb41ad6aaaf1cc7a89d0d8d83
ThinkPad <angelinazhukovich@gmail.com>
2023-02-04 15:39:13 +0300

    worked with string at home

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 const text = 'hello world';
 
 console.log(text);
+
+const String = 'diedie   jdeijdiejdei'
+// const String1 = new String(name);
+// console.log(String1)
+// console.log(name.indexOf('j', 5))
+// console.log(name.lastIndexOf('j'))
+console.log(String.length)
+console.log(String.padEnd(25, '?'))
+console.log(String.padStart(25, '?'))
+console.log(String.repeat(5))
+console.log(String.split('  ')) // разбивает на слова
+console.log(String.split('')) /// на буквы
+console.log(String.split()) // просто массив
+// console.log(String.fromCharCode(189,43,190,61));
+// console.log(String.fromCodePoint())
+
+
+// const getText = (name) => `My name is ${name}`
+// console.log(getText(`Angelina`))
+
+// const s_prim= 'foo';
+// const s_obj = new String(s_prim);
+// console.log(s_obj);
+// // если дописать toString
+// console.log(s_obj.toString()); // обратно получим строку 'foo'
+
+//
+// // eslint-disable-next-line no-unused-vars
+// const word = 'cat';
+// console.log('word'.charAt (0)); // выводит word -w-
+// console.log(word.charAt(0)); // выводит word -c-
